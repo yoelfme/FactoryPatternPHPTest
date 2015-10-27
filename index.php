d1eb8b39bd47d0dd480fb2cc74592d2a34011efc
Yoel Monzon <yoelfme@hotmail.com>
2015-10-27 12:41:21 -0600

    [add] authentication service class

--- index.php
+++ index.php
@@ -1,21 +1,47 @@
 <?php 
 
+session_start();
+$_SESSION['user'] = [];
+$_SESSION['user']['name'] = 'Yoel';
+
 require ('User.php');
 require ('Coffee.php');
 
+class AuthenticationService
+{
+    private $sessionData;
+    public function __construct(array $sessionData = array())
+    {
+        $this->sessionData = $sessionData;
+    }
+
+    public function user()
+    {
+        return new User($this->sessionData);   
+    }
+}
+
 class Controller 
 {
+    private $auth;
+
+    public function __construct(AuthenticationService $auth)
+    {
+        $this->auth = $auth;
+    }
+
     public function action()
     {
-        $user = new User(['name' => 'Yoel']);
+        $user = $this->auth->user();
         $coffee = new Coffee();
 
         $message = $user->drink($coffee);
 
-        echo "<h1>$message</h1>";
+        require 'view.php';
     }
 }
 
-$controller = new Controller();
+$auth = new AuthenticationService($_SESSION['user']);
+$controller = new Controller($auth);
 
 $controller->action();
---
+++ view.php
@@ -0,0 +1,3 @@
+<h1>
+    <?php echo $message; ?>
+</h1>
